77e16d848f0cfc2d3ac967a41ed6ba130debd480
MAshokM2007 <94159105+MAshokM2007@users.noreply.github.com>
2021-11-12 11:05:18 +0530

    Add files via upload

---
+++ Lucky Number.py
@@ -0,0 +1,9 @@
+LN=int(input("Enter lucky number"))
+if LN == 12 :
+    print("yes 12 is lucky number")
+elif (LN <= 10):
+    print("to Low")
+elif LN >= 15 :
+    print("to high")
+elif LN > 10 and LN<15 :
+    print ("almost there")
